e656207cbd5b27b49d0447898911ebf072c5d6f4
Rajwardhan Patil <rajpatil2k@gmail.com>
2024-04-17 11:54:18 +0530

    socket change

--- src/socket.jsx
+++ src/socket.jsx
@@ -9,18 +9,6 @@ const getSocket = () => useContext(SocketContext);
 const SocketProvider = ({ children }) => {
   const socket = useMemo(() => io(server, { withCredentials: true }), []);
 
-  useEffect(() => {
-    // Handle socket connection errors
-    socket.on("error", (error) => {
-      console.error("Socket connection error:", error);
-    });
-
-    return () => {
-      // Cleanup function to close socket connection
-      socket.close();
-    };
-  }, [socket]);
-
   return (
     <SocketContext.Provider value={socket}>
       {children}
